49b0690a137295ac23be8498eee780424fed5fe0
Seth DeKrey <rook512@gmail.com>
2019-05-15 19:55:44 -0500

    Added city class + tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,3 @@ const App: React.FC = () => {
   );
 }
 
-export default App;
---
+++ src/cities/City.test.ts
@@ -0,0 +1,5 @@
+import {City} from './City';
+
+it('creates a new city without crashing', () => {
+  const city = new City();
+});
