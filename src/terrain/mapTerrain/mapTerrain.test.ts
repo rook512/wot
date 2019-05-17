f57b0dc8dc211c74500ed7caa6153e978a4b8968
Seth DeKrey <rook512@gmail.com>
2019-05-16 19:38:18 -0500

    Added Map Terrain Class

---
+++ src/terrain/mapTerrain/mapTerrain.test.ts
@@ -0,0 +1,5 @@
+import {MapTerrain} from './mapTerrain';
+
+it('creates a new Map Terrain without crashing', () => {
+  const race = new MapTerrain("Anything");
+});
---
+++ src/terrain/mapTerrain/mapTerrain.ts
@@ -0,0 +1,5 @@
+export class MapTerrain {
+    readonly name:string
+    constructor(name: string) {
+        this.name = name}
+}
